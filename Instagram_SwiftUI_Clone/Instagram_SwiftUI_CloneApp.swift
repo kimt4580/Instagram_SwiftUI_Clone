eeb1db196777d1a5dfe33dc225d942b339cbe0f5
kimt4580 <xogns8696@gmail.com>
2023-10-11 18:56:37 +0900

    initial setting

---
+++ Instagram_SwiftUI_Clone/Instagram_SwiftUI_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUI_CloneApp.swift
+//  Instagram_SwiftUI_Clone
+//
+//  Created by 김태훈 on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_SwiftUI_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
